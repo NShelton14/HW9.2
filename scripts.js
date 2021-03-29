d8f20525d33cc1c2c62adccd050734f70dfce031
Nathan Shelton <nathan.shelton@student.swic.edu>
2021-03-29 04:22:49 -0500

    HW9.2 Dom 1 Changes

--- scripts.js
+++ scripts.js
@@ -1,1 +1,12 @@
+document.querySelector("#username").addEventListener("focus", () => {
+  console.log("Entered input!");
+});
+(method)console.log(message?: any, ...optionalParams: any[]): void(+2 overloads)
 
+document.querySelector("#username").addEventListener("blur", () => {
+  console.log("Left input!");
+});
+
+document.querySelector("form").addEventListener("submit", () => {
+  console.log("Submitted form!");
+});
